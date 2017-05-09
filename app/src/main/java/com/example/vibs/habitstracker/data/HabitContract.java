c5c0eb7518c2e4fbcae7f6e5ab89e3fae99f1d40
vibhakarsaraswat <vibhakar.saraswat@gmail.com>
2017-05-09 21:51:00 +0200

    Added 'HabitContract.java': 1. Added a new Contract class under ‘data’ package i.e. ‘HabitContract.java’ class.     1.1 This ‘HabitContract.java’ class consists an Empty Constructor and     1.2 An Inner Class i.e. HabitEntry which implemets BaseColumns class.     1.3 'HabitEntry' Class defines constant values for the dailyhabits database table.

---
+++ app/src/main/java/com/example/vibs/habitstracker/data/HabitContract.java
@@ -0,0 +1,67 @@
+package com.example.vibs.habitstracker.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by vibhakar.sarswat on 5/9/2017.
+ */
+
+public class HabitContract {
+
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    public HabitContract() {
+    }
+
+    // Inner class that defines constant values for the dailyhabits database table.
+    // Each entry in the table represents User Habits for a single Day.
+    public static final class HabitEntry implements BaseColumns {
+
+        /** Name of database table for User Habits */
+        public final static String TABLE_NAME = "dailyhabits";
+
+        // Unique ID number for the pet (only for use in the database table).
+        // Type: INTEGER
+        public final static String _ID = BaseColumns._ID;
+
+        // Date: date to records habits for a single day.
+        // Type: INTEGER
+        public final static String COLUMN_HABIT_DATE ="date";
+
+        /**
+         * Exercise: The only possible values are {@link #EXERCISE_YES} or {@link #EXERCISE_NO}
+         * Type: INTEGER
+         */
+        public final static String COLUMN_HABIT_EXCERCISE ="excercise";
+
+        /**
+         * Breakfast: The only possible values are {@link #BREAKFAST_YES} or {@link #BREAKFAST_NO}
+         * Type: INTEGER
+         */
+        public final static String COLUMN_HABIT_BREAKFAST ="breakfast";
+
+        /**
+         * Smoking: The only possible values are {@link #SMOKING_YES} or {@link #SMOKING_NO}
+         * Type: INTEGER
+         */
+        public final static String COLUMN_HABIT_SMOKING ="smoking";
+
+        // SleepHours: No of hours user slept in a single day.
+        // Type: INTEGER
+        public static final String COLUMN_NAME_SLEEPHOURS = "sleepHours";
+
+
+
+        // Possible values for the excercise habit.
+        public static final int EXERCISE_YES = 0;
+        public static final int EXERCISE_NO = 1;
+
+        // Possible values for the breakfast habit.
+        public static final int BREAKFAST_YES = 0;
+        public static final int BREAKFAST_NO = 1;
+
+        // Possible values for the smoking habit.
+        public static final int SMOKING_YES = 0;
+        public static final int SMOKING_NO = 1;
+    }
+}
